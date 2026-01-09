9a7b3406fdc00003f25537c8978d38e415dce369
vivek2034 <gamervivek3004@gmail.com>
2026-01-09 12:29:08 +0530

    Update route.js

--- app/api/notes/route.js
+++ app/api/notes/route.js
@@ -13,15 +13,3 @@ export async function POST(req) {
   const note = await Note.create(data);
   return Response.json(note);
 }
-export async function PUT(req, { params }) {
-  await connectDB();
-  const data = await req.json();
-  const note = await Note.findByIdAndUpdate(params.id, data, { new: true });
-  return Response.json(note);
-}
-
-export async function DELETE(req, { params }) {
-  await connectDB();
-  await Note.findByIdAndDelete(params.id);
-  return Response.json({ message: "Note Deleted" });
-}
